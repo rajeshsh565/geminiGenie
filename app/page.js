aa6028b5afbb178e48df87420cf8cf5482550cbc
Rajesh Singh <rajeshsh565@gmail.com>
2024-08-17 15:02:01 +0530

    landingFix

--- app/page.js
+++ app/page.js
@@ -2,17 +2,13 @@ import Link from "next/link";
 
 const HomePage = () => {
   return (
-    <div className="hero min-h-screen bg-base-200">
-      <div className="hero-content text-center">
-        <div className="max-w-md">
-          <h1 className="text-7xl text-primary font-extrabold">GeminiGenie</h1>
-          <p className="my-5 leading-loose text-lg">
+    <div className="flex flex-col h-screen w-screen bg-base-200 justify-center items-center text-center px-4">
+          <h1 className="text-4xl md:text-7xl text-primary font-extrabold">GeminiGenie</h1>
+          <p className="my-5 leading-loose text-lg max-w-md">
             Your AI language companion. Powered by Vertex AI, it
             enhances your conversations, content creation, and more!
           </p>
           <Link href="/sign-in" className="btn btn-secondary">Let's Go!</Link>
-        </div>
-      </div>
     </div>
   );
 };
